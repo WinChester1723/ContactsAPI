ceb6e4273a4894d18d37f67aefc7afbd510d49b0
Pasha Pashazade <qordon23@gmail.com>
2023-03-17 16:50:07 +0400

    add sql server

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -22,6 +22,19 @@ namespace ContactsAPI.Controllers
             //return View();
         }
 
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContact([FromRoute]Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(contact);
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
         {
@@ -42,9 +55,38 @@ namespace ContactsAPI.Controllers
 
         [HttpPut]
         [Route("{id:guid}")]
-        public async Task<IActionResult> UpdateContact([FromRoute]Guid id, )
+        public async Task<IActionResult> UpdateContact([FromRoute]Guid id, UpdateContatcRequest updateContatcRequest)
         {
+            var contact = await dbContext.Contacts.FindAsync(id);
 
+            if (contact != null)
+            {
+                contact.FullName = updateContatcRequest.FullName;
+                contact.Address = updateContatcRequest.Address;
+                contact.Phone = updateContatcRequest.Phone;
+                contact.Email = updateContatcRequest.Email;
+
+                await dbContext.SaveChangesAsync();
+
+                return Ok(contact);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute]Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+
+            if(contact != null)
+            {
+                dbContext.Contacts.Remove(contact);
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+
+            return NotFound();
         }
     }
 }
